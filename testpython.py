5ab572a8de97821c2c8bb653c6189e5ed925b708
matildamuotka <102036888+matildamuotka@users.noreply.github.com>
2022-12-11 18:56:56 +0100

    Update testpython.py

--- testpython.py
+++ testpython.py
@@ -1,1 +1,57 @@
-print("hi")
+# -*- coding: utf-8 -*-
+
+from datetime import datetime,date,time,timezone,timedelta
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pytz
+from matplotlib import pyplot as plt
+from sqlalchemy import create_engine #to access a sql database
+
+
+# pd.read_csv(r"G:\My Drive\Madrid\COURS\TBDA\TBDATEAM\data.csv")
+# uploaded_file = st.file_uploader("Fill out the project plan template and upload your file here. After you upload the file, you can edit your project plan within the app.", type=['csv'])
+
+
+engine = create_engine('postgresql://lectura:ncorrea#2022@138.100.82.178:5432/2207')
+
+# Recuperation of all operating modes
+OPmodes = pd.read_sql_query("select id_var, value from variable_log_float where id_var=622 limit 1000", con=engine)
+list1=OPmodes["value"]
+x= np.array(list1)
+op_modes = np.unique(x).astype(str)[:-1]
+def query(x):
+    return pd.read_sql_query("".join(["select id_var, date, to_timestamp(@date/1000) as dateH, value from variable_log_float where id_var=622 and value=",x]),con=engine)
+modes = [query(x) for x in op_modes]
+
+st.title("Histogram per operating mode")
+
+# Choice of the time window
+start_date = st.date_input("Start date",date(2020,12,28),date(2020,12,28),date(2022,2,23))
+start_time = st.time_input("Start time",time(6,00,00))
+end_date = st.date_input("End date",date(2020,12,28),date(2020,12,28),date(2022,2,23))
+end_time = st.time_input("End time",time(22,00,00))
+
+start_datetime = datetime.combine(start_date,start_time,tzinfo=timezone(timedelta(seconds=3600)))
+end_datetime = datetime.combine(end_date,end_time,tzinfo=timezone(timedelta(seconds=3600)))
+
+#Number of minutes in the interval
+time_interval = end_datetime - start_datetime
+time_interval_min = time_interval.seconds/60
+
+
+# operations collected and operations per minute
+counters_collected = []
+counters_per_min = []
+
+
+for mode in modes:
+    op_collected = mode[mode['dateh'] > start_datetime][mode['dateh'] < end_datetime]['dateh'].size
+    counters_collected.append(op_collected)
+    counters_per_min.append(op_collected/time_interval_min)
+
+st.text("Collected operations by operating mode")
+st.bar_chart(counters_collected)
+
+st.text("Collected operations per minute by operating mode")
+st.bar_chart(counters_per_min)
