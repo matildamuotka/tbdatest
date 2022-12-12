d7bab558336be8314c1c094e5fe7726b32ce40c6
matildamuotka <102036888+matildamuotka@users.noreply.github.com>
2022-12-12 18:29:39 +0100

    Update testpython.py

--- testpython.py
+++ testpython.py
@@ -2,7 +2,9 @@ from datetime import datetime,date,time,timezone,timedelta
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly
 import plotly.graph_objects as go
+import plotly.express as px
 from matplotlib import pyplot as plt
 from sqlalchemy import create_engine #to access a sql database
 
@@ -62,3 +64,13 @@ fig = go.Figure(go.Gantt(
 
 # Display the chart
 st.plotly_chart(fig)
+
+df = pd.DataFrame([
+    dict(Task="Job A", Start='2009-01-01', Finish='2009-02-28'),
+    dict(Task="Job B", Start='2009-03-05', Finish='2009-04-15'),
+    dict(Task="Job C", Start='2009-02-20', Finish='2009-05-30')
+])
+
+fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task")
+fig.update_yaxes(autorange="reversed") # otherwise tasks are listed from the bottom up
+fig.show()
