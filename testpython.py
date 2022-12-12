f5f3f2f39441ea5240d6bd0d103bc72e205fb81e
matildamuotka <102036888+matildamuotka@users.noreply.github.com>
2022-12-12 17:47:24 +0100

    Update testpython.py

--- testpython.py
+++ testpython.py
@@ -1,15 +1,9 @@
-# -*- coding: utf-8 -*-
-
 from datetime import datetime,date,time,timezone,timedelta
 import streamlit as st
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from sqlalchemy import create_engine #to access a sql database
-
-# pd.read_csv(r"G:\My Drive\Madrid\COURS\TBDA\TBDATEAM\data.csv")
-# uploaded_file = st.file_uploader("Fill out the project plan template and upload your file here. After you upload the file, you can edit your project plan within the app.", type=['csv'])
-
 
 engine = create_engine('postgresql://lectura:ncorrea#2022@138.100.82.178:5432/2207')
 
