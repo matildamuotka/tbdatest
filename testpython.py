b55ee7d70fc59c4c4c87e23f0fd48eeed2d3c42d
matildamuotka <102036888+matildamuotka@users.noreply.github.com>
2022-12-12 19:22:09 +0100

    Update testpython.py

--- testpython.py
+++ testpython.py
@@ -68,11 +68,13 @@ running = []
 for i in range(len(values)):
     if (values[i] == 255.0): # If the value is 255, we put a 0 a the same time exactly to have a square chart
         new_values.append(0)
+        running.append(0)
         new_dates.append(dates[i])
         new_values.append(1)
         running.append(1)
     elif (values[i] == 0.0): # If the value is 0, we put a 1 a the same time exactly to have a square chart
         new_values.append(1)
+        running.append(1)
         new_dates.append(dates[i])
         new_values.append(0)
         running.append(0)
