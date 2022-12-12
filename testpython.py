fd323a960b29f3d507e43ae309eb95f9ace831f7
matildamuotka <102036888+matildamuotka@users.noreply.github.com>
2022-12-12 18:44:30 +0100

    Update testpython.py

--- testpython.py
+++ testpython.py
@@ -54,4 +54,4 @@ df = pd.DataFrame([
 
 fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task")
 fig.update_yaxes(autorange="reversed") # otherwise tasks are listed from the bottom up
-st.show(fig)
+st.plotly_chart(fig)
